95b713ad94ed0f7b30a614db6b88bace2f50c77c
0-void <muthukumarperiyasamy.mk@gmail.com>
2021-04-02 06:58:37 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/css/tailwind.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 import { MDXProvider } from '@mdx-js/react'
 import { ThemeProvider } from 'next-themes'
 import { DefaultSeo } from 'next-seo'
@@ -10,6 +14,16 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import MDXComponents from '@/components/MDXComponents'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
